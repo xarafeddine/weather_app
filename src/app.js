f284e6cafc92cb352d2093a826f8b0d8d160c400
xarafeddine <nu01280@gmail.com>
2022-02-08 13:04:25 +0100

    add my name"

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Xaraf'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Xaraf'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Xaraf'
     })
 })
 
@@ -97,7 +97,7 @@ app.get('/products', (req,res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Xaraf',
         errorMessage: 'Help article not found.'
     })
 })
@@ -105,7 +105,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Xaraf',
         errorMessage: 'Page not found.'
     })
 })
