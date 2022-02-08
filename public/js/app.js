2e3acdb0d866b26923e40973446f016c5808460b
xarafeddine <nu01280@gmail.com>
2022-02-08 13:11:18 +0100

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then(res => {
+    fetch(`/weather?address=${location}`).then(res => {
         res.json().then((data) => {
             if(data.error){
                 messageOne.textContent = data.error
